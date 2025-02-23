dd93a0c8be99dea56b16cd4fcc3fe1a0ddc38851
Te0-faN <simerea.teofan@cnprsv.ro>
2025-02-23 17:59:30 +0200

    update la motion profiler si sper ca vi se va compila si voua

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/math/TrapezoidalMotionProfiler.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/math/TrapezoidalMotionProfiler.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.math;
 
 import com.qualcomm.robotcore.util.ElapsedTime;
+
 import static org.firstinspires.ftc.teamcode.Constants.MAX_VEL;
 import static org.firstinspires.ftc.teamcode.Constants.MAX_ACC;
 
@@ -10,58 +11,40 @@ class TrapeziodalMotionProfiler {
 
     double[] calculate(double dist)
     {
-        if (!init)
+        if (!init) {
             timer.reset();
-        // Calculate the time it takes to accelerate to max velocity
-      double accel_dt = MAX_VEL / MAX_ACC;
+            init = true;
+        }
+        double t = timer.seconds();
+        double deltat_amax = MAX_VEL / MAX_ACC;
+        double dist_amax  = 0.5 * MAX_ACC * deltat_amax * deltat_amax;
+        double v_max = MAX_VEL;
 
-      // If we can't accelerate to max velocity in the given dist, we'll accelerate as much as possible
-      double accel_dist = 0.5 * MAX_ACC * accel_dt * accel_dt;
+        if (dist_amax > 0.5*dist) {
+            deltat_amax = Math.sqrt(dist / MAX_ACC);
+            dist_amax = 0.5 * MAX_ACC * deltat_amax * deltat_amax;
+            v_max = MAX_ACC * deltat_amax;
+        }
 
-      if (accel_dist > (dist / 2))
-        accel_dt = Math.sqrt((dist / 2) / (0.5 * MAX_ACC));
+        double dist_cruize = dist - 2 * dist_amax;
+        double deltat_cruize = dist_cruize / v_max;
 
-      accel_dist = 0.5 * MAX_ACC * accel_dt * accel_dt;
+        double deltat_tot = 2 * deltat_amax + deltat_cruize;
+        if (t >= deltat_tot)
+            return new double[] { dist };
 
-      // recalculate max velocity based on the time we have to accelerate and decelerate
-      double max_vel = MAX_ACC * accel_dt;
+        if (t < deltat_amax) {
+             return new double[] { 0.5 * MAX_ACC * t * t, MAX_ACC * t, MAX_ACC };
+        } else if (t < deltat_amax + deltat_cruize) {
+            return new double[] { dist_amax + v_max * (t - deltat_amax), v_max, 0 };
+        } else {
+            double deltat_decel = t - deltat_amax - deltat_cruize;
+            return new double[] {
+                    dist_amax + v_max * deltat_cruize + v_max * deltat_decel - 0.5 * MAX_ACC * deltat_decel * deltat_decel,
+                    v_max - MAX_ACC * deltat_decel,
+                   -MAX_ACC
+            };
+        }
 
-      // we decelerate at the same rate as we accelerate
-      double decel_dt = accel_dt;
-
-      // calculate the time that we're at max velocity
-        double cruise_dist = dist - 2 * accel_dist;
-        double cruise_dt = cruise_dist / max_vel;
-        double decel_time = accel_dt + cruise_dt;
-
-      // check if we're still in the motion profile
-      double entire_dt = accel_dt + cruise_dt + decel_dt;
-      if (timer.seconds() > entire_dt) {
-        return new double[] { dist };
-      }
-
-      // if we're accelerating
-      if (timer.seconds() < accel_dt)
-        // use the kinematic equation for accel
-          return new double[]{ 0.5 * MAX_ACC * timer.seconds() * timer.seconds(), max_vel, MAX_ACC };
-
-      // if we're cruising
-      else if (timer.seconds() < decel_time) {
-        accel_dist = 0.5 * MAX_ACC * accel_dt * accel_dt;
-        double cruise_current_dt = timer.seconds() - accel_dt;
-
-        // use the kinematic equation for constant velocity
-          return new double[] {accel_dist + MAX_VEL * cruise_current_dt , max_vel, MAX_ACC};
-      }
-
-      // if we're decelerating
-      else {
-        accel_dist = 0.5 * MAX_ACC * accel_dt * accel_dt;
-        cruise_dist = max_vel * cruise_dt;
-        decel_time = timer.seconds() - decel_time;
-
-        // use the kinematic equations to calculate the instantaneous desired position
-          return new double[] { accel_dist + cruise_dist + max_vel * decel_time - 0.5 * MAX_ACC * decel_time * decel_time, max_vel, MAX_ACC };
-      }
     }
 }
